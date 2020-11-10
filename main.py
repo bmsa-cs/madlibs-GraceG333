a8616f5aa3d9b19438ba7c94e5695b1603a77b98
Grace Gongaware <ggongaware24@biomedscienceacademy.org>
2020-11-10 13:56:59 +0000

    I added the \n

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
+Author: Grace Gongaware 
+Period/Core: 5
 
 """
+
+print("Lets play Silly Sentences!")
+
+name = input("Enter a persons name: ")
+name_two = input("Enter another person name: ")
+place = input("Enter a place to take a trip: ")
+positive_adjective = input("Enter an adjective: ")
+name_three = input("Enter another persons name: ")
+color = input("Enter a color: ")
+time_of_day = input("Enter a time: ")
+favorite_food = input("Enter your favorite_food: ")
+restaurant_name = input("Enter a restaurant name: ")
+adjective_two = input("Enter another adjective: ")
+
+print(name+ " and " +name_two+ " were planning on taking a trip to " +place+ "." "\nThey were so " +positive_adjective+ " to take a trip and have some fun. They \nwere planning on taking their friend " +name_three+ " with them on \nthe trip. " +name+ "'s favorite color was " +color+ " so they made \nsure that they packed many " +color+ " outfits. ") 
+
+print("When they got to " +place+ " at " +time_of_day+ " they were starving. Their \nfavorite food was " +favorite_food+ " so when they landed they went to a \]\nrestaurant called " +restaurant_name+ " to get their favorite food. ")
+
+print("After " +name+ " and their friends got done with eating they \nwere " +adjective_two+ " to explore around. " +name+ " was happy they had the chance to come to " +place+ " and see this amazing place. ")
