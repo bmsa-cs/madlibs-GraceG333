da3363833932a842fd2445a981304a4a7ded42ee
Grace Gongaware <ggongaware24@biomedscienceacademy.org>
2020-11-10 16:00:40 +0000

    I incorporated the f string in my final code.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 MadLibs
 Author: Grace Gongaware 
 Period/Core: 5
-
 """
 
 print("Lets play Silly Sentences!")
@@ -10,16 +9,13 @@ print("Lets play Silly Sentences!")
 name = input("Enter a persons name: ")
 name_two = input("Enter another person name: ")
 place = input("Enter a place to take a trip: ")
-positive_adjective = input("Enter an adjective: ")
+positive_adjective = input("Enter a positive adjective: ")
 name_three = input("Enter another persons name: ")
 color = input("Enter a color: ")
 time_of_day = input("Enter a time: ")
-favorite_food = input("Enter your favorite_food: ")
+favorite_food = input("Enter your favorite food: ")
 restaurant_name = input("Enter a restaurant name: ")
 adjective_two = input("Enter another adjective: ")
 
-print(name+ " and " +name_two+ " were planning on taking a trip to " +place+ "." "\nThey were so " +positive_adjective+ " to take a trip and have some fun. They \nwere planning on taking their friend " +name_three+ " with them on \nthe trip. " +name+ "'s favorite color was " +color+ " so they made \nsure that they packed many " +color+ " outfits. ") 
-
-print("When they got to " +place+ " at " +time_of_day+ " they were starving. Their \nfavorite food was " +favorite_food+ " so when they landed they went to a \]\nrestaurant called " +restaurant_name+ " to get their favorite food. ")
-
-print("After " +name+ " and their friends got done with eating they \nwere " +adjective_two+ " to explore around. " +name+ " was happy they had the chance to come to " +place+ " and see this amazing place. ")
+print(f"{name} and {name_two} were planning on taking a trip to {place}. They were so {positive_adjective} to take a trip and have some fun. They were planning on taking their friend {name_three} with them on the trip. {name}'s favorite color is {color} so they made sure that they packed many {color} outfits.\nWhen they got to {place} at {time_of_day} they were starving. Their favorite food was {favorite_food} so when they landed they went to a restaurant called {restaurant_name} to get their favorite food.")
+print(" After " +name+ " and their friends got done with eating they were " +adjective_two+ " to explore the town. " +name+ " was happy they had the chance to come to " +place+ " and see this amazing place. ")
